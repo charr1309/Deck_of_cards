c3aa07363769917e07f6b3566458639ce1df5559
charr1309 <ceh710@aol.com>
2021-05-23 03:22:51 -0400

    updats to card.js, deck.js, created card and deck.css

--- deck_of_cards/src/Card.js
+++ deck_of_cards/src/Card.js
@@ -1,10 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./Card.css";
 
 class Card extends Component {
-    render() {
-        return (
-            <img clasName='Card'src={this.props.image} alt={this.props.name}/>
-        )
-    }
+  render() {
+    let angle = Math.random() * 90 - 45;
+    let xPos = Math.random() * 40 - 20;
+    let yPos = Math.random() * 40 - 20;
+    let transform = `translate(${xPos}px, ${yPos}px rotate(${angle})deg`;
+    return (
+      <img
+        style={{ transform: transform }}
+        className="Card"
+        src={this.props.image}
+        alt={this.props.name}
+      />
+    );
+  }
 }
- export default Card;
+export default Card;
